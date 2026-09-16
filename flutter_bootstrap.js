f8bb6207abb578bc2c55e76b2d9668c1468a7951
AndhikaW <andhikaw61@gmail.com>
2026-09-16 10:38:53 +0700

    ci: add automatic deployment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "872903505" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3680089525" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
